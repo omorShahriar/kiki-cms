48381809f2d30a1dc1c5c9344dca8ff5e86c97d0
omorShahriar <omor.shahriar2000@gmail.com>
2023-03-13 14:34:40 +0600

    page same slug for locale

---
+++ src/api/page/content-types/page/lifecycles.js
@@ -0,0 +1,30 @@
+let slugify = require("slugify");
+const { ApplicationError } = require("@strapi/utils").errors;
+
+module.exports = {
+  async beforeCreate(event) {
+    await generateSlug(event);
+  },
+
+  async beforeUpdate(event) {
+    await generateSlug(event);
+  },
+};
+
+const generateSlug = async (event) => {
+  const DEFAULT_LOCALE = "en";
+  const { data } = event.params;
+  const id = event.params?.where?.id ?? null;
+  const locale = !id ? "en" : await getLocale(id);
+
+  //Generate slug for en locale only
+  if (!data.slug && data.title && locale == DEFAULT_LOCALE) {
+    data.slug = slugify(data.title, { lower: true });
+  }
+};
+
+const getLocale = async (id) => {
+  const res = await strapi.service("api::page.page").findOne(id);
+
+  return res?.locale ?? "not found";
+};
